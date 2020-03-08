a8f667a2235d8b0d0f7a32cd7c381a8205cd4785
jgenso1690 <jimenagensollen@gmail.com>
2020-03-08 11:03:20 -0400

    Adding cardlength function to Maestro Test

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -174,6 +174,7 @@ describe('Maestro', function() {
  var num = 5678901
   for (var cardlength = 12;cardlength<20 ;cardlength++ ){
 	num += '1';
+	(function(cardlength){
   it(`has a prefix of 5018 and a length of ${cardlength}`, function() {
     detectNetwork('5018' + num ).should.equal('Maestro');
    });
@@ -186,5 +187,7 @@ describe('Maestro', function() {
    it(`has a prefix of 6304 and a length of ${cardlength}`, function() {
     detectNetwork('6304' + num).should.equal('Maestro');
     });
+})(cardlength)
  };
+
 });
