7dc41f695de43345b60459638a3090ea9d5340b0
jgenso1690 <jimenagensollen@gmail.com>
2020-03-07 20:25:33 -0500

    Added Exceptions for VISA AND MASTERCARD

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,11 +11,14 @@ var detectNetwork = function(cardNumber) {
 	var prefix = cardNumber[0]+cardNumber[1];
   
 		if (cardNumber.length === 14 && (prefix === "38" || prefix === "39" )){
-      
-		return "Diner's Club";
-	}else if(cardNumber.length === 15 && (prefix === "34" || prefix === "37")){
+     return "Diner's Club";
+    }else if(cardNumber.length === 15 && (prefix === "34" || prefix === "37")){
 	return "American Express";
-}
+    }else if(cardNumber.length === 16 && (prefix >= 51 && prefix <= 55)){
+      return "MasterCard"
+    }else if((cardNumber.length === 13 ||cardNumber.length === 16 ||cardNumber.length === 19) && (cardNumber[0] === "4")){
+      return "Visa"
+    }
 
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
