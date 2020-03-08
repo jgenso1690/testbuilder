deec9558b6beffe72d55743edfacbbc79052060a
jgenso1690 <jimenagensollen@gmail.com>
2020-03-07 21:51:55 -0500

    amended code for maestro and discover

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,22 +8,27 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-	var prefix = cardNumber[0]+cardNumber[1];
-  var prefix2 = cardNumber[0]+cardNumber[1]+cardNumber[2]+cardNumber[3]
+	var prefix1 = cardNumber.slice(0,1);
+  var prefix2 = cardNumber.slice(0,2);
+  var prefix3 = cardNumber.slice(0,3);
+  var prefix4 = cardNumber.slice(0,4);
   
-		if (cardNumber.length === 14 && (prefix === "38" || prefix === "39" )){
+  
+		if (cardNumber.length === 14 && (prefix2 === "38" || prefix2 === "39" )){
      return "Diner's Club";
-    }else if(cardNumber.length === 15 && (prefix === "34" || prefix === "37")){
+    }else if(cardNumber.length === 15 && (prefix2 === "34" || prefix2 === "37")){
 	return "American Express";
-    }else if(cardNumber.length === 16 && (prefix >= 51 && prefix <= 55)){
+    }else if(cardNumber.length === 16 && (prefix2 >= 51 && prefix2 <= 55)){
       return "MasterCard"
-    }else if((cardNumber.length === 13 ||cardNumber.length === 16 ||cardNumber.length === 19) && (cardNumber[0] === "4")){
+    }else if((cardNumber.length === 13 ||cardNumber.length === 16 ||cardNumber.length === 19) && (prefix1 === "4")){
       return "Visa"
-    }else if((cardNumber.length === 16 ||cardNumber.length === 19) && (prefix2 === "6011")){
-      return "Discover"
-    }else if((cardNumber.length === 16 ||cardNumber.length === 19) && (prefix2 === "6011")){
+    }else if ((prefix2 === 65 || prefix4 === 6011 || (prefix3 >= 644 && prefix3 <= 649)) && (cardNumber.length === 16 || cardNumber.length === 19)){
+    return 'Discover'
+    }else if((cardNumber.length >= 12 && cardNumber.length <= 19) && (prefix4 === "6011"||prefix4 === "5018"||prefix4 === "5020"||prefix4 === "5038"||prefix4 === "6304")){
       return "Maestro"
     }
+    
+    return "Not a valid number";
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
