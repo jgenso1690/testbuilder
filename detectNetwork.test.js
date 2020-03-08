b43799651532cf011ac2ea21fb1b1ca96a854a02
jgenso1690 <jimenagensollen@gmail.com>
2020-03-07 22:02:13 -0500

    mocha test mod1

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -17,10 +17,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // Once you've read and understood this section, please comment it out. 
   // You will not be able to proceed with a failing test. 
 
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
-
+  
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
     // This test doesn't really test anything at all! It will pass no matter what.
     var even = function(num){
