a968850dee5c9ad5d0550f1c63463ac7ffff0bd2
jgenso1690 <jimenagensollen@gmail.com>
2020-03-07 19:24:59 -0500

    Code added to detect creditcard

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,6 +8,10 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+	if (cardNumber.length === 14){
+	return "Diner's CLub";
+	}else{
+	return "American Express";
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
